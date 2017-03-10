11b503eda97a788196dc2853fbf90fd21ea78f87
George Fawcett <georgealanfawcett@gmail.com>
2017-03-10 16:48:33 +0000

    Import/export exercises

---
+++ callmodule.js
@@ -0,0 +1,5 @@
+var myModule = require("./testmodule");
+
+console.log(myModule.testvalue);
+console.log(myModule.PHI);
+console.log(myModule.newvar);
---
+++ testmodule.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testvalue: 5,
+  PHI: 1.618,
+  explain: function() {
+    console.log("(X+Y) is to X as X is to Y: (X+Y)≈" + this.PHI);
+  }
+};
+
+var newvar = 10;
